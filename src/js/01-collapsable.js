36f5ade877f0004f4fbe0386ba69f1c0ae0f3302
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-08 21:51:25 +0200

    change js collapsable

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,43 +1,28 @@
 "use strict";
-
 const collapsableDesign = document.querySelector(".js-collapsable-design");
 const collapsableFill = document.querySelector(".js-collapsable-fill");
 const collapsableShare = document.querySelector(".js-collapsable-share");
-const arrowUp = document.querySelector(".fa-chevron-up");
-const arrowDown = document.querySelector(".fa-chevron-down");
+const arrow1 = document.querySelector(".js-arrow1");
+const arrow2 = document.querySelector(".js-arrow2");
+const arrow3 = document.querySelector(".js-arrow3");
 
 function collapsableArrowDesign() {
-  if (collapsableDesign.classList.contains("collapsable-hidden")) {
-    collapsableDesign.classList.toggle("collapsable-hidden");
-    arrowDown.classList.toggle("fa-chevron-up");
-  } else {
-    collapsableDesign.classList.toggle("collapsable-hidden");
-    arrowDown.classList.toggle("fa-chevron-up");
-  }
+  collapsableDesign.classList.toggle("collapsable-hidden");
+  arrow1.classList.toggle("fa-chevron-up");
+  arrow1.classList.toggle("fa-chevron-down");
 }
-arrowUp.addEventListener("click", collapsableArrowDesign);
-arrowDown.addEventListener("click", collapsableArrowDesign);
+arrow1.addEventListener("click", collapsableArrowDesign);
 
 function collapsableArrowFill() {
-  if (collapsableFill.classList.contains("collapsable-hidden")) {
-    collapsableFill.classList.toggle("collapsable-hidden");
-    arrowUp.classList.toggle("fa-chevron-up");
-  } else {
-    collapsableFill.classList.toggle("collapsable-hidden");
-    arrowUp.classList.toggle("fa-chevron-up");
-  }
+  collapsableFill.classList.toggle("collapsable-hidden");
+  arrow2.classList.toggle("fa-chevron-up");
+  arrow2.classList.toggle("fa-chevron-down");
 }
-arrowUp.addEventListener("click", collapsableArrowFill);
-arrowDown.addEventListener("click", collapsableArrowFill);
+arrow2.addEventListener("click", collapsableArrowFill);
 
 function collapsableArrowShare() {
-  if (collapsableShare.classList.contains("collapsable-hidden")) {
-    collapsableShare.classList.toggle("collapsable-hidden");
-    arrowUp.classList.toggle("fa-chevron-down");
-  } else {
-    collapsableShare.classList.toggle("collapsable-hidden");
-    arrowUp.classList.toggle("fa-chevron-down");
-  }
+  collapsableShare.classList.toggle("collapsable-hidden");
+  arrow3.classList.toggle("fa-chevron-up");
+  arrow3.classList.toggle("fa-chevron-down");
 }
-arrowUp.addEventListener("click", collapsableArrowShare);
-arrowDown.addEventListener("click", collapsableArrowShare);
+arrow3.addEventListener("click", collapsableArrowShare);

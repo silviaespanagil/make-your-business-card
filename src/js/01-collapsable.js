132ca6215d88d8400b9f95feefdcb1a304f0beb4
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-11 20:00:45 +0200

    change coments javascript collapsables

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,32 +1,37 @@
 "use strict";
 
+// Constantes Collapsables
 const collapsableDesign = document.querySelector(".js-collapsable-design");
 const collapsableFill = document.querySelector(".js-collapsable-fill");
 const collapsableShare = document.querySelector(".js-collapsable-share");
+// Constantes Flechas Collapsables
 const arrow1 = document.querySelector(".js-arrow1");
 const arrow2 = document.querySelector(".js-arrow2");
 const arrow3 = document.querySelector(".js-arrow3");
+// Constante borde inferior collapsables
 const borderHeader = document.querySelector(".border-hidden");
-borderHeader.classList.remove("border-hidden");
 
+// Collapsable Design
 function collapsableArrowDesign() {
   collapsableDesign.classList.toggle("collapsable-hidden");
   arrow1.classList.toggle("fa-chevron-up");
   arrow1.classList.toggle("fa-chevron-down");
   borderHeader.classList.toggle("border-hidden");
 }
-arrow1.addEventListener("click", collapsableArrowDesign);
-
+// Collapsable Fill
 function collapsableArrowFill() {
   collapsableFill.classList.toggle("collapsable-hidden");
   arrow2.classList.toggle("fa-chevron-up");
   arrow2.classList.toggle("fa-chevron-down");
 }
-arrow2.addEventListener("click", collapsableArrowFill);
-
+// Collapsable Share
 function collapsableArrowShare() {
   collapsableShare.classList.toggle("collapsable-hidden");
   arrow3.classList.toggle("fa-chevron-up");
   arrow3.classList.toggle("fa-chevron-down");
 }
+
+// Eventos collapsables
+arrow1.addEventListener("click", collapsableArrowDesign);
+arrow2.addEventListener("click", collapsableArrowFill);
 arrow3.addEventListener("click", collapsableArrowShare);

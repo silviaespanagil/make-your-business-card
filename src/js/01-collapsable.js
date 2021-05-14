434e2958446b6b5cffa381583890c2caadbc2f5e
Almoyano <a.moyano.miguel@gmail.com>
2021-05-14 19:24:25 +0200

    Change js collapsible

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,16 +1,15 @@
-/* eslint-disable */
-
 "use strict";
 
 // Constantes Collapsables
 const collapsableDesign = document.querySelector(".js-collapsable-design");
 const collapsableFill = document.querySelector(".js-collapsable-fill");
 const collapsableShare = document.querySelector(".js-collapsable-share");
+
 // Constantes Flechas Collapsables
 const arrow = document.querySelector(".js-arrow");
+const collapsableHeader = document.querySelectorAll(".js-collapsable-header");
 
-//Collapsable
-
+// Collapsable
 function collapsableArrow(a) {
   a.classList.toggle("collapsable-hidden");
   arrow.classList.toggle("arrow-rotate");
@@ -22,5 +21,9 @@ function handlerCollapsable() {
   collapsableArrow(collapsableShare);
 }
 
-// Eventos collapsables
-arrow.addEventListener("click", handlerCollapsable);
+// // Bucle para escuchar cada flecha dentro del colapsable header
+// for (const element of section) {
+//   arrow.addEventListener("click", handlerCollapsable);
+// }
+
+// //¿Cómo escuchamos el evento sobre el elemento header? ¿Con event.target, solo con el bucle...?

dd7c92d932e5c0dc43acf9b4d69b9911c46c1553
janadim <jana.d.titarchuk@gmail.com>
2021-05-15 13:24:35 +0200

    refactor palette

--- src/js/02-palette.js
+++ src/js/02-palette.js
@@ -1,4 +1,5 @@
-/* eslint-disable strict */
+/*eslint-disable*/
+// "use strict";
 
 const paletteElement = document.querySelector(".collapsable-design__colors");
 
@@ -11,27 +12,37 @@ const cardDataSocialColor = document.querySelectorAll(
 );
 const cardDataBorderColor = document.querySelector(".preview__card-data");
 
+const cardDataPallet =
+  (cardDataNameColor, cardDataProfColor, cardDataBorderColor);
+const colorsPalette1 = (changeNameColor1, changeProfColor1, changeBorderColor1);
+const colorsPalette2 =
+  ("changeNameColor2", "changeProfColor2", "changeBorderColor2");
+const colorsPalette3 =
+  ("changeNameColor3", "changeProfColor3", "changeBorderColor3");
+console.log(colorsPalette1);
+function setColor(cardElement, addPallet, removePallet1, removePallet2) {
+  cardElement.classList.add(addPallet);
+  cardElement.classList.remove(removePallet1, removePallet2);
+}
+
 function changePaletteColor(event) {
   let target = event.target;
+
   switch (target.id) {
     case "palette1":
-      cardDataNameColor.classList.add("changeNameColor1");
-      cardDataNameColor.classList.remove(
-        "changeNameColor2",
-        "changeNameColor3"
-      );
+      setColor(cardDataPallet, colorsPalette1, colorsPalette2, colorsPalette3);
 
-      cardDataProfColor.classList.add("changeProfColor1");
-      cardDataProfColor.classList.remove(
-        "changeProfColor2",
-        "changeProfColor3"
-      );
+      // cardDataProfColor.classList.add("changeProfColor1");
+      // cardDataProfColor.classList.remove(
+      //   "changeProfColor2",
+      //   "changeProfColor3"
+      // );
 
-      cardDataBorderColor.classList.add("changeBorderColor1");
-      cardDataBorderColor.classList.remove(
-        "changeBorderColor2",
-        "changeBorderColor3"
-      );
+      // cardDataBorderColor.classList.add("changeBorderColor1");
+      // cardDataBorderColor.classList.remove(
+      //   "changeBorderColor2",
+      //   "changeBorderColor3"
+      // );
 
       cardDataSocialColor.forEach((element) =>
         element.classList.add("changeSocialColor1")
@@ -41,23 +52,24 @@ function changePaletteColor(event) {
       );
       break;
     case "palette2":
-      cardDataNameColor.classList.add("changeNameColor2");
-      cardDataNameColor.classList.remove(
-        "changeNameColor3",
-        "changeNameColor1"
-      );
+      setColor(cardDataPallet, colorsPalette2, colorsPalette1, colorsPalette3);
+      // cardDataNameColor.classList.add("changeNameColor2");
+      // cardDataNameColor.classList.remove(
+      //   "changeNameColor3",
+      //   "changeNameColor1"
+      // );
 
-      cardDataProfColor.classList.add("changeProfColor2");
-      cardDataProfColor.classList.remove(
-        "changeProfColor1",
-        "changeProfColor3"
-      );
+      // cardDataProfColor.classList.add("changeProfColor2");
+      // cardDataProfColor.classList.remove(
+      //   "changeProfColor1",
+      //   "changeProfColor3"
+      // );
 
-      cardDataBorderColor.classList.add("changeBorderColor2");
-      cardDataBorderColor.classList.remove(
-        "changeBorderColor1",
-        "changeBorderColor3"
-      );
+      // cardDataBorderColor.classList.add("changeBorderColor2");
+      // cardDataBorderColor.classList.remove(
+      //   "changeBorderColor1",
+      //   "changeBorderColor3"
+      // );
 
       cardDataSocialColor.forEach((element) =>
         element.classList.add("changeSocialColor2")
@@ -67,23 +79,24 @@ function changePaletteColor(event) {
       );
       break;
     case "palette3":
-      cardDataNameColor.classList.add("changeNameColor3");
-      cardDataNameColor.classList.remove(
-        "changeNameColor1",
-        "changeNameColor2"
-      );
+      setColor(cardDataPallet, colorsPalette3, colorsPalette1, colorsPalette2);
+      // cardDataNameColor.classList.add("changeNameColor3");
+      // cardDataNameColor.classList.remove(
+      //   "changeNameColor1",
+      //   "changeNameColor2"
+      // );
 
-      cardDataProfColor.classList.add("changeProfColor3");
-      cardDataProfColor.classList.remove(
-        "changeProfColor1",
-        "changeProfColor2"
-      );
+      // cardDataProfColor.classList.add("changeProfColor3");
+      // cardDataProfColor.classList.remove(
+      //   "changeProfColor1",
+      //   "changeProfColor2"
+      // );
 
-      cardDataBorderColor.classList.add("changeBorderColor3");
-      cardDataBorderColor.classList.remove(
-        "changeBorderColor1",
-        "changeBorderColor2"
-      );
+      // cardDataBorderColor.classList.add("changeBorderColor3");
+      // cardDataBorderColor.classList.remove(
+      //   "changeBorderColor1",
+      //   "changeBorderColor2"
+      // );
 
       cardDataSocialColor.forEach((element) =>
         element.classList.add("changeSocialColor3")

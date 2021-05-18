424d2905261f14c27380169cc97237623210cb11
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-18 14:57:06 +0200

    change border bottom header collapsable section

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -5,24 +5,24 @@ const collapsableDesign = document.querySelector(".js-collapsable-design");
 const collapsableFill = document.querySelector(".js-collapsable-fill");
 const collapsableShare = document.querySelector(".js-collapsable-share");
 
-// Constantes Flechas Collapsables
+// Constante Flechas Collapsables
 const arrows = document.querySelectorAll(".js-arrow");
-const collapsableHeader = document.querySelectorAll(".js-collapsable-header");
-
-const collapsableTotal = [collapsableDesign, collapsableFill, collapsableShare];
 
 // Collapsable
 function collapsableArrow(event) {
   console.log(event.target.parentElement);
   const sectionToCollapse = event.target.parentElement.nextElementSibling;
+  console.log(event.target.parentElement.nextElementSibling);
 
   collapsableDesign.classList.add("collapsable-hidden");
   collapsableFill.classList.add("collapsable-hidden");
   collapsableShare.classList.add("collapsable-hidden");
   sectionToCollapse.classList.remove("collapsable-hidden");
+
+  arrows.classList.toggle("arrow-rotate");
 }
 
 // // Bucle para escuchar cada flecha
-for (const arrow of arrows) {
+for (let arrow of arrows) {
   arrow.addEventListener("click", collapsableArrow);
 }

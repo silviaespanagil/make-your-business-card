f7520f90f86295cc4823316b93a0ff265382a217
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-05-18 17:44:36 +0200

    change js collapsable

--- src/js/01-collapsable.js
+++ src/js/01-collapsable.js
@@ -1,28 +1,43 @@
 "use strict";
 
-// Constantes Collapsables
+// Constantes Secciones Colapsables
 const collapsableDesign = document.querySelector(".js-collapsable-design");
 const collapsableFill = document.querySelector(".js-collapsable-fill");
 const collapsableShare = document.querySelector(".js-collapsable-share");
 
-// Constante Flechas Collapsables
+// Constante Flechas
 const arrows = document.querySelectorAll(".js-arrow");
 
-// Collapsable
-function collapsableArrow(event) {
-  console.log(event.target.parentElement);
-  const sectionToCollapse = event.target.parentElement.nextElementSibling;
-  console.log(event.target.parentElement.nextElementSibling);
+// Funcion collapsable
+function collapsable(event) {
+  // constante seccion que debe descollapsar
+  const sectionClick = event.target.parentElement.nextElementSibling;
 
+  // Añade clase hidden para ocultar todas las secciones
   collapsableDesign.classList.add("collapsable-hidden");
   collapsableFill.classList.add("collapsable-hidden");
   collapsableShare.classList.add("collapsable-hidden");
-  sectionToCollapse.classList.remove("collapsable-hidden");
 
-  arrows.classList.toggle("arrow-rotate");
+  // Quita la clase hidden a la clase clickada para mostrarse
+  sectionClick.classList.remove("collapsable-hidden");
 }
 
-// // Bucle para escuchar cada flecha
+/*function rotateArrow(event) {
+  const arrowClick = event.target;
+
+  if (sectionClick.classList.contains("collapsable-hidden")) {
+    arrowClick.classList.remove("arrow-rotate");
+  } else if (-----) {
+    arrowClick.classList.add("arrow-rotate");
+  }
+}*/
+
+function handlerCollapsable(event) {
+  collapsable(event);
+  /*rotateArrow(event);*/
+}
+
+// Bucle para escuchar cada flecha
 for (let arrow of arrows) {
-  arrow.addEventListener("click", collapsableArrow);
+  arrow.addEventListener("click", handlerCollapsable);
 }

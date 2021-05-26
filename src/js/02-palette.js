712311215c8f84d7064ae7d800e28d83cc18a59c
janadim <jana.d.titarchuk@gmail.com>
2021-05-26 17:07:55 +0200

    add changes palette dot

--- src/js/02-palette.js
+++ src/js/02-palette.js
@@ -1,9 +1,16 @@
 const cardDataPallet = document.querySelector(".preview__card");
 const paletteElement = document.querySelector(".collapsable-design__colors");
 const paletteSelector = document.querySelector(".radio-class");
+const palettesRadios = document.querySelectorAll(".radio-class");
 
+// for (const paletteRadio of palettesRadios) {
+//   if (paletteSelector.value === 3) {
+//     paletteRadio.setAttribute("checked", "true");
+//   }
+// }
 function changePaletteColor(event) {
   let target = event.target;
+
   cardDataPallet.classList.remove("palcol1", "palcol2", "palcol3");
   switch (target.id) {
     case "palette1":
@@ -13,27 +20,27 @@ function changePaletteColor(event) {
     case "palette2":
       cardDataPallet.classList.add("palcol2");
       data["palette"] = 2;
+
       break;
     case "palette3":
       cardDataPallet.classList.add("palcol3");
       data["palette"] = 3;
+
       break;
   }
   setlocalHost();
 }
+
 function previewPalette() {
   if (data.palette === 1) {
     cardDataPallet.classList.remove("palcol2", "palcol3");
-    cardDataPallet.classList.add("palcol1");
-    //paletteSelector.checked === true;
+    cardDataPallet.classList.add("palcol1", "pal1");
   } else if (data.palette === 2) {
     cardDataPallet.classList.remove("palcol1", "palcol3");
-    cardDataPallet.classList.add("palcol2");
-    //paletteSelector.checked === true;
+    cardDataPallet.classList.add("palcol2", "pal2");
   } else if (data.palette === 3) {
     cardDataPallet.classList.remove("palcol1", "palcol2");
-    cardDataPallet.classList.add("palcol3");
-    //paletteSelector.checked === true;
+    cardDataPallet.classList.add("palcol3", "pal3");
   }
 }
 

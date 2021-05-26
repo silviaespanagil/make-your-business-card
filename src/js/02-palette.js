468536bf1fb5653416e0ad80b3e441f1ae0de70c
janadim <jana.d.titarchuk@gmail.com>
2021-05-26 18:46:51 +0200

    merge styles

--- src/js/02-palette.js
+++ src/js/02-palette.js
@@ -17,18 +17,19 @@ function changePaletteColor(event) {
   cardDataPallet.classList.remove("palcol1", "palcol2", "palcol3");
   switch (target.id) {
     case "palette1":
-      cardDataPallet.classList.add("palcol1");
+      cardDataPallet.classList.add("palcol1", "input-checked");
       data["palette"] = 1;
+      // palettesRadios.classList.add("input-checked");
       break;
     case "palette2":
-      cardDataPallet.classList.add("palcol2");
+      cardDataPallet.classList.add("palcol2", "input-checked");
       data["palette"] = 2;
-
+      // palettesRadios.classList.add("input-checked");
       break;
     case "palette3":
-      cardDataPallet.classList.add("palcol3");
+      cardDataPallet.classList.add("palcol3", "input-checked");
       data["palette"] = 3;
-
+      // palettesRadios.classList.add("input-checked");
       break;
   }
   setlocalHost();

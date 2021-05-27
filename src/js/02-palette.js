4d34a1deb739c0f3ced4ab5f9a51955aad2262ca
janadim <jana.d.titarchuk@gmail.com>
2021-05-27 12:14:49 +0200

    fix palette radios after refreshing

--- src/js/02-palette.js
+++ src/js/02-palette.js
@@ -1,15 +1,12 @@
 const cardDataPallet = document.querySelector(".preview__card");
 const paletteElement = document.querySelector(".collapsable-design__colors");
 const paletteSelector = document.querySelector(".radio-class");
-const palettesRadios = document.querySelectorAll(".radio-class");
+const label1 = document.querySelector(".label1 input");
+const label2 = document.querySelector(".label2 input");
+const label3 = document.querySelector(".label3 input");
 
-// for (const paletteRadio of palettesRadios) {
-//   if (paletteSelector.value === 3) {
-//     paletteRadio.setAttribute("checked", "true");
-//   }
-// }
-const paletteChecked = document.querySelector(".input-checked");
-paletteChecked.checked = true;
+const paletteDefault = document.querySelector(".js-paletteDefault");
+paletteDefault.checked = true;
 
 function changePaletteColor(event) {
   let target = event.target;
@@ -17,19 +14,16 @@ function changePaletteColor(event) {
   cardDataPallet.classList.remove("palcol1", "palcol2", "palcol3");
   switch (target.id) {
     case "palette1":
-      cardDataPallet.classList.add("palcol1", "input-checked");
+      cardDataPallet.classList.add("palcol1");
       data["palette"] = 1;
-      // palettesRadios.classList.add("input-checked");
       break;
     case "palette2":
-      cardDataPallet.classList.add("palcol2", "input-checked");
+      cardDataPallet.classList.add("palcol2");
       data["palette"] = 2;
-      // palettesRadios.classList.add("input-checked");
       break;
     case "palette3":
-      cardDataPallet.classList.add("palcol3", "input-checked");
+      cardDataPallet.classList.add("palcol3");
       data["palette"] = 3;
-      // palettesRadios.classList.add("input-checked");
       break;
   }
   setlocalHost();
@@ -39,12 +33,15 @@ function previewPalette() {
   if (data.palette === 1) {
     cardDataPallet.classList.remove("palcol2", "palcol3");
     cardDataPallet.classList.add("palcol1", "pal1");
+    label1.setAttribute("checked", "");
   } else if (data.palette === 2) {
     cardDataPallet.classList.remove("palcol1", "palcol3");
     cardDataPallet.classList.add("palcol2", "pal2");
+    label2.setAttribute("checked", "");
   } else if (data.palette === 3) {
     cardDataPallet.classList.remove("palcol1", "palcol2");
     cardDataPallet.classList.add("palcol3", "pal3");
+    label3.setAttribute("checked", "");
   }
 }
 
